74d2a0c3702032d165352fc667e3ec80ecca47d3
Anton <23635473+1progon@users.noreply.github.com>
2022-10-24 12:00:12 +0400

    Add domain url link to env files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://dotnetcase.com/api/v1/'
+  apiUrl: 'https://dotnetcase.com/api/v1/',
+  domainUrl: 'https://dotnetcase.com/',
 };
